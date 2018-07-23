f050a06b9ca99312f9ad1b1e12f81f96fc475150
heislercreative <brennan@heislecreative.com>
2018-07-22 18:56:38 -0600

    comment out registerServiceWorker

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.css';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+// registerServiceWorker();
