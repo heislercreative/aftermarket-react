eddd93cfdd92d890e1c6c505c76ea3304b7ab55d
heislercreative <brennan@heislecreative.com>
2018-07-22 19:23:22 -0600

    generate Cart & UserProduct models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :user_products
+  resources :carts
   resources :users
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
---
+++ db/migrate/20180723012242_create_user_products.rb
@@ -0,0 +1,10 @@
+class CreateUserProducts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :user_products do |t|
+      t.integer :user_id
+      t.integer :product_id
+
+      t.timestamps
+    end
+  end
+end
