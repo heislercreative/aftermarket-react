66b7429ba72d4e32e5ba53932755087f8a0c35f1
heislercreative <brennan@heislecreative.com>
2018-07-24 20:50:03 -0600

    add user validators

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
-  has_secure_password
   has_one :cart
+
+  has_secure_password
+  validates_presence_of :email, :password, :first_name, :last_name, :address, :city, :state, :zip
+  validates :state, length: { maximum: 2 }
+  validates :zip, length: { is: 5 }
 
   def self.new_with_cart(params)
     user = User.new(params)
