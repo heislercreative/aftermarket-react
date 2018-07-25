79349af871042e883cc579901eda75c3214fb55b
heislercreative <brennan@heislecreative.com>
2018-07-24 21:26:43 -0600

    set up index.js (comment out store for now)

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'semantic-ui-css/semantic.css';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider, createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import 'semantic-ui-css/semantic.css'
+import './index.css'
+// import rootReducer from './reducers'
+import App from './App'
 // import registerServiceWorker from './registerServiceWorker';
 
+// const store = createStore(rootReducer, applyMiddleware(thunk))
+
 ReactDOM.render(
-  <App />,
+  // <Provider store={store} >
+    <App />,
+  // </Provider>,
   document.getElementById('root')
 );
 // registerServiceWorker();
