07a5b69bb8c9f9fc1c908f4f5b12401a21d8d8dd
heislercreative <brennan@heislecreative.com>
2018-07-27 18:43:41 -0600

    extracted form to AccountForm component

---
+++ client/src/components/AccountForm.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import { Form, Input, Button } from 'semantic-ui-react'
+import StatesList from './StatesList'
+
+class AccountForm extends Component {
+  render(){
+    return(
+      <Form>
+        <Input className='form-input' label='Email' placeholder='account@aftermarket.com' /><br/>
+        <Input className='form-input' label='Password' placeholder='••••••••' type='password'/><br/>
+        <Input className='form-input' label='First Name' placeholder='John' /><br/>
+        <Input className='form-input' label='Last Name' placeholder='Doe' /><br/>
+        <Input className='form-input' label='Address' placeholder='123 Main St' /><br/>
+        <Input className='form-input' label='City' placeholder='Durnsville' /><br/>
+        <Input className='form-input' list='states' label='State' placeholder='IN' />
+          <StatesList /><br/>
+        <Input className='form-input' label='Zip Code' placeholder='12345' width={2} /><br/><br/>
+        <Button primary type='submit'>Sign Up</Button>
+      </Form>
+    )
+  }
+}
+
+export default AccountForm
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
-import { Header, Container, Form, Label, Input, Button, Divider } from 'semantic-ui-react'
-import StatesList from './StatesList'
+import { Header, Container, Button, Divider } from 'semantic-ui-react'
+import AccountForm from './AccountForm'
+
 
 class Signup extends Component {
   render() {
@@ -10,18 +11,7 @@ class Signup extends Component {
           Create a New Account
           <Divider hidden />
         </Header>
-        <Form>
-          <Input className='form-input' label='Email' placeholder='account@aftermarket.com' /><br/>
-          <Input className='form-input' label='Password' placeholder='••••••••' type='password'/><br/>
-          <Input className='form-input' label='First Name' placeholder='John' /><br/>
-          <Input className='form-input' label='Last Name' placeholder='Doe' /><br/>
-          <Input className='form-input' label='Address' placeholder='123 Main St' /><br/>
-          <Input className='form-input' label='City' placeholder='Durnsville' /><br/>
-          <Input className='form-input' list='states' label='State' placeholder='IN' />
-            <StatesList /><br/>
-          <Input className='form-input' label='Zip Code' placeholder='12345' width={2} /><br/><br/>
-          <Button primary type='submit'>Sign Up</Button>
-        </Form>
+        <AccountForm />
         <Divider horizontal section>OR</Divider>
         <Button secondary>Log In</Button>
       </Container>
