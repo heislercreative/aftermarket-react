75caecec364f334c04146cb5c2c18a3b884564eb
heislercreative <brennan@heislecreative.com>
2018-08-01 06:24:57 -0600

    use StatesList component & change form styling

--- client/src/components/AccountForm.js
+++ client/src/components/AccountForm.js
@@ -86,85 +86,37 @@ class AccountForm extends Component {
               onChange={this.handleChange}
             />
           </Form.Field>
-          <Form.Field>
-            <label>City</label>
-            <input
-              name='city'
-              type='text'
-              placeholder='Durnsville'
-              value={this.state.city}
-              onChange={this.handleChange}
-            />
-          </Form.Field>
           <Form.Group>
+            <Form.Field className='city-input'>
+              <label>City</label>
+              <input
+                name='city'
+                type='text'
+                placeholder='Durnsville'
+                value={this.state.city}
+                onChange={this.handleChange}
+              />
+            </Form.Field>
             <Form.Field className='state-input'>
               <label>State</label>
-              <select
+              <input
                 name='stateInitials'
                 className='ui fluid dropdown'
+                list='states'
+                placeholder='IN'
+                maxlength='2'
                 value={this.state.stateInitials}
                 onChange={this.handleChange}
-              >
-                <option value=""></option>
-                <option value="AL">Alabama</option>
-              	<option value="AK">Alaska</option>
-              	<option value="AZ">Arizona</option>
-              	<option value="AR">Arkansas</option>
-              	<option value="CA">California</option>
-              	<option value="CO">Colorado</option>
-              	<option value="CT">Connecticut</option>
-              	<option value="DE">Delaware</option>
-              	<option value="DC">District Of Columbia</option>
-              	<option value="FL">Florida</option>
-              	<option value="GA">Georgia</option>
-              	<option value="HI">Hawaii</option>
-              	<option value="ID">Idaho</option>
-              	<option value="IL">Illinois</option>
-              	<option value="IN">Indiana</option>
-              	<option value="IA">Iowa</option>
-              	<option value="KS">Kansas</option>
-              	<option value="KY">Kentucky</option>
-              	<option value="LA">Louisiana</option>
-              	<option value="ME">Maine</option>
-              	<option value="MD">Maryland</option>
-              	<option value="MA">Massachusetts</option>
-              	<option value="MI">Michigan</option>
-              	<option value="MN">Minnesota</option>
-              	<option value="MS">Mississippi</option>
-              	<option value="MO">Missouri</option>
-              	<option value="MT">Montana</option>
-              	<option value="NE">Nebraska</option>
-              	<option value="NV">Nevada</option>
-              	<option value="NH">New Hampshire</option>
-              	<option value="NJ">New Jersey</option>
-              	<option value="NM">New Mexico</option>
-              	<option value="NY">New York</option>
-              	<option value="NC">North Carolina</option>
-              	<option value="ND">North Dakota</option>
-              	<option value="OH">Ohio</option>
-              	<option value="OK">Oklahoma</option>
-              	<option value="OR">Oregon</option>
-              	<option value="PA">Pennsylvania</option>
-              	<option value="RI">Rhode Island</option>
-              	<option value="SC">South Carolina</option>
-              	<option value="SD">South Dakota</option>
-              	<option value="TN">Tennessee</option>
-              	<option value="TX">Texas</option>
-              	<option value="UT">Utah</option>
-              	<option value="VT">Vermont</option>
-              	<option value="VA">Virginia</option>
-              	<option value="WA">Washington</option>
-              	<option value="WV">West Virginia</option>
-              	<option value="WI">Wisconsin</option>
-              	<option value="WY">Wyoming</option>
-              </select>
+              />
+                <StatesList />
             </Form.Field>
-            <Form.Field>
+            <Form.Field className='zip-input'>
               <label>Zip Code</label>
               <input
                 name='zip'
                 type='text'
                 placeholder='12345'
+                maxlength='5'
                 value={this.state.zip}
                 onChange={this.handleChange}
               />
