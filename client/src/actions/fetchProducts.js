6364069e0abff524c68299a8bd4c14c5836432f1
heislercreative <brennan@heislecreative.com>
2018-08-01 19:38:48 -0600

    create fetchProducts action

---
+++ client/src/actions/fetchProducts.js
@@ -0,0 +1,10 @@
+// import fetch from 'isomorphic-fetch'
+
+export function fetchProducts() {
+    return (dispatch) => {
+      dispatch({ type: 'LOADING_PRODUCTS' })
+      return fetch(`api/products`)
+        .then(resp => resp.json())
+        .then(products => dispatch({ type: 'FETCH_PRODUCTS', payload: products.images }))
+    }
+}
