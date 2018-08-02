6bfb97325d3a1482f4deda5ae0d9f34fcc9d860c
heislercreative <brennan@heislecreative.com>
2018-08-01 19:28:52 -0600

    add rootReducer, Provider, & store back to index

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Provider, createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import 'semantic-ui-css/semantic.css'
 import './index.css'
-// import rootReducer from './reducers'
+import rootReducer from './reducers/rootReducer'
 import App from './App'
 // import registerServiceWorker from './registerServiceWorker';
 
-// const store = createStore(rootReducer, applyMiddleware(thunk))
+const store = createStore(rootReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
-  // <Provider store={store} >
-    <App />,
-  // </Provider>,
+  <Provider store={store} >
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
 // registerServiceWorker();
