6f97cdeba10dd3ac9386a362f06babcfd9f4d2d7
heislercreative <brennan@heislecreative.com>
2018-08-02 18:22:40 -0600

    add link to whole ProductBasic component

--- client/src/components/ProductBasic.js
+++ client/src/components/ProductBasic.js
@@ -3,11 +3,13 @@ import React, { Component } from 'react'
 class ProductBasic extends Component {
   render() {
     return (
-      <div className='product-div'>
-        <img className='thumbnail-img' src={this.props.thumbnail} />
-        <a href='#'><h4>{this.props.name}</h4></a>
-        <strong>${this.props.price}</strong>
-      </div>
+      <a className='product-link' href='#'>
+        <div className='product-div'>
+          <img className='thumbnail-img' src={this.props.thumbnail} />
+          <h4>{this.props.name}</h4>
+          <strong>${this.props.price}</strong>
+        </div>
+      </a>
     )
   }
 }
