0a74ba25cc23b93a15ea45bb72c8cf103e93a1ed
heislercreative <brennan@heislecreative.com>
2018-08-05 11:08:18 -0600

    downcase email before validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,15 @@ class User < ApplicationRecord
   has_one :cart
   has_secure_password
 
-  # before_validation {
-  #   self.email.to_s.downcase
-  # }
-
+  before_validation :downcase_email
   validates_presence_of :email, :password, :first_name, :last_name, :address, :city, :state_initials, :zip
-  # validates_uniqueness_of :email
+  validates_uniqueness_of :email
   validates :state_initials, length: { maximum: 2 }
   validates :zip, length: { is: 5 }
+
+  def downcase_email
+    self.email.downcase!
+  end
 
   def self.new_with_cart(params)
     user = User.new(params)
