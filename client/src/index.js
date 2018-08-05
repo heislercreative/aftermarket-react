7e3b1f7daec103323a21ac60f57d1379359177c9
heislercreative <brennan@heislecreative.com>
2018-08-05 15:56:30 -0600

    save store to local storage via Redux-Persist

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2'
+import rootReducer from './reducers/rootReducer'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import 'semantic-ui-css/semantic.css'
 import './index.css'
-import rootReducer from './reducers/rootReducer'
 import App from './App'
 // import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
+const persistConfig = {
+  key: 'root',
+  storage: storage,
+  stateReconciler: autoMergeLevel2
+}
+
+const pReducer = persistReducer(persistConfig, rootReducer)
+
+const store = createStore(pReducer, applyMiddleware(thunk))
+export const persistor = persistStore(store)
 
 ReactDOM.render(
   <Provider store={store} >
-    <App />
+      <App />
   </Provider>,
   document.getElementById('root')
 );
