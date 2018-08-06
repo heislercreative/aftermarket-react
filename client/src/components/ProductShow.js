08564d9526808df849997c139a824f499c2774e6
heislercreative <brennan@heislercreative.com>
2018-08-06 06:50:02 -0600

    access productId param for ProductShow component;

--- client/src/components/ProductShow.js
+++ client/src/components/ProductShow.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-const ProductShow = () => {
+const ProductShow = ({ productId }) => {
   return (
     <div className='product-div'>
-      <h3>Test Content</h3>
+      <h3>Test #{productId}</h3>
+
+      <em>$so</em>
     </div>
   )
 }
 
-export default ProductShow
+const mapStateToProps = (state, ownProps) => {
+  return {
+    userId: state.user.id,
+    productId: ownProps.match.params.productId
+  }
+}
+
+export default connect(mapStateToProps)(ProductShow)
