751730c7cf678a3386d2a4664d1f2c12279ece8f
heislercreative <brennan@heislercreative.com>
2018-08-21 06:41:30 -0600

    change from class to const format

--- client/src/components/Account.js
+++ client/src/components/Account.js
@@ -1,20 +1,15 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Header, Container, Divider } from 'semantic-ui-react'
 import AccountForm from './AccountForm'
 
 
-class Signup extends Component {
-  render() {
-    return(
-      <Container>
-        <Header as='h2' textAlign='center'>
-          Update Account Details
-          <Divider hidden />
-        </Header>
-        <AccountForm formType={'updateUser'} />
-      </Container>
-    )
-  }
-}
+const Signup = () =>
+  <Container>
+    <Header as='h2' textAlign='center'>
+      Update Account Details
+      <Divider hidden />
+    </Header>
+    <AccountForm formType={'updateUser'} />
+  </Container>
 
 export default Signup
