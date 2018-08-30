a9318ec74ef5e80b03e6bb66ca958e3b140a7ad3
heislercreative <brennan@heislecreative.com>
2018-08-29 18:35:37 -0600

    change from API to Base and turn off CSRF token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
+  skip_before_action :verify_authenticity_token
+
   def fallback_index_html
     render :file => '/public/index.html'
   end
+  
 end
