192fa96908b7e299154e75d89f076d046573290f
heislercreative <brennan@heislercreative.com>
2019-04-02 17:38:19 -0600

    show login button instead of add to cart

--- client/src/components/ProductShow.js
+++ client/src/components/ProductShow.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import { Button } from 'semantic-ui-react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import * as actions from '../actions/productActions'
@@ -24,7 +25,14 @@ class ProductShow extends Component {
         </div>
         <div className='price-and-description'>
           <h3><em>${product.price}</em></h3>
-          { this.props.cartId && <AddToCart cartId={this.props.cartId} productId={this.props.productId}/>}
+          { this.props.token &&
+            <AddToCart cartId={this.props.cartId} productId={this.props.productId}/>
+          }
+          { !this.props.token &&
+            <Link to='/login'>
+              <Button primary className='login-button'>Log In to Add to Cart</Button>
+            </Link>
+          }
           <p dangerouslySetInnerHTML={{__html: product.description}} />
         </div>
       </div>
@@ -39,6 +47,7 @@ class ProductShow extends Component {
 function mapStateToProps(state, ownProps) {
   if (state.user.hasOwnProperty('cart')) {
     return {
+      token: state.user.token,
       cartId: state.user.cart.id,
       productId: ownProps.match.params.productId,
       product: state.currentProduct
